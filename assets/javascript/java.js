9803b4273943b88237718973d564983dd5045695
Whittle <matt.whittle10@gmail.com>
2019-07-13 21:11:53 -0600

    projects linked and basic layout completed

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,100 +1,99 @@
+$(document).ready(function(){
+  $("#appendArea").empty();
+    $("#appendArea").append(
+    "<h2 class='title'>About Me</h2>" +
+    "<br>" +
+    "<p>I was born and raised in Kaysville Utah. I grew up in a family of 4 kids, two boys and two girls. We grew up going camping a lot and enjoying the outdoors. Growing up I remember always go fourwheeling and camping in the mountains for the majority of our family vacations.</p><br>" + 
+    "<p>I've been married for 5 years now and currently have two little boys that I love spending time with. Some of my hobbies are playing soccer, fishing, riding motorcycles, and restoring and fixing classic cars & motorcycles.</p><br>" +
+    "<p>Currently I work at Fidelty Invesmtents. I have been working there for the past 4 years and have enjoyed it but am looking to pursue a career in software development and web design.</p><br>" 
+    )
+});
+
 $("#about").on('click', function(){
     $("#appendArea").empty();
-    $("#appendArea").append("<p>I was born and raised in Kaysville Utah. I grew up in a family of 4 kids, two boys and two girls. We grew up going camping a lot and enjoying the outdoors. Growing up I remember always go fourwheeling and camping in the mountains for the majority of our family vacations.</p><br><br>" + 
-    "<p>I got married in July of 2014 and currently have two little boys that I love spending time with. We love to go fishing together. Our first boy is 2 years old and the second was just born. Some of my hobbies are playing soccer, fishing, riding motorcycles, and restoring and fixing classic cars & motorcycles.</p><br><br>" +
-    "<p>Currently I work at Fidelty Invesmtents. I have been working there for the past 4 years and have enjoyed it but am looking to pursue a career in software engineering and web design.</p><br>" 
+    $("#appendArea").append(
+    "<h2 class='title'>About Me</h2>" +
+    "<br>" +
+    "<p>I was born and raised in Kaysville Utah. I grew up in a family of 4 kids, two boys and two girls. We grew up going camping a lot and enjoying the outdoors. Growing up I remember always go fourwheeling and camping in the mountains for the majority of our family vacations.</p><br><br>" + 
+    "<p>I've been married for 5 years now and currently have two little boys that I love spending time with. Some of my hobbies are playing soccer, fishing, riding motorcycles, and restoring and fixing classic cars & motorcycles.</p><br><br>" +
+    "<p>Currently I work at Fidelty Invesmtents. I have been working there for the past 4 years and have enjoyed it but am looking to pursue a career in software development and web design.</p><br>" 
     )
 });
 
 $("#projects").on('click', function(){
     $("#appendArea").empty();
-    $("#appendArea").append("<div class='card-group'>" +
+    $("#appendArea").append(
+      "<h2 class='title'>My Projects</h2>" +
+      "<br>" +
+    "<div class='card-deck'>" +
     "<div class='card text-center' class='card'>" +
-      "<img class='card-img-top' src='...' alt='Card image cap'>" +
+      "<a href='./projects/ProjectTest/index.html' target='_blank'><img class='card-img-top' src='./assets/images/trailProject.jpg' alt='Card image cap'></a>" +
       "<div class='card-body'>" +
-        "<h5 class='card-title'>Card title</h5>" +
-        "<p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>" +
+        "<h5 class='card-title'>National Parks Project</h5>" +
       "</div>" +
-      "<div class='card-footer'>" +
-        "<small class='text-muted'>Last updated 3 mins ago</small>" +
-      "</div>" +
+      
     "</div>" +
     "<div class='card text-center' class='card'>" +
-      "<img class='card-img-top' src='...' alt='Card image cap'>" +
+      "<a href='./projects/word-guess-game/index.html' target='_blank'><img class='card-img-top' src='./assets/images/hangman.jpg' alt='Card image cap'></a>" +
       "<div class='card-body'>" +
-        "<h5 class='card-title'>Card title</h5>" +
-        "<p class='card-text'>This card has supporting text below as a natural lead-in to additional content.</p>" +
+        "<h5 class='card-title'>Hangman Game</h5>" +
       "</div>" +
-      "<div class='card-footer'>" +
-        "<small class='text-muted'>Last updated 3 mins ago</small>" +
-      "</div>" +
+      
     "</div>" +
     "<div class='card text-center' class='card'>" +
-      "<img class='card-img-top' src='...' alt='Card image cap'>" +
+      "<a href='./projects/unit-4-game-final/index.html' target='_blank'><img class='card-img-top' src='./assets/images/crystalGame.jpg' alt='Card image cap'></a>" +
       "<div class='card-body'>" +
-        "<h5 class='card-title'>Card title</h5>" +
-        "<p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>" +
+        "<h5 class='card-title'>Crystal Counter</h5>" +
       "</div>" +
-      "<div class='card-footer'>" +
-        "<small class='text-muted'>Last updated 3 mins ago</small>" +
-      "</div>" +
+      
     "</div>" +
   "</div>" +
   "<br>" +
-  "<hr>" +
+  "<hr class='line'>" +
   "<br>" +
-  "<div class='card-group'>" +
+  "<div class='card-deck'>" +
         "<div class='card text-center' class='card'>" +
-          "<img class='card-img-top' src='...' alt='Card image cap'>" +
+          "<a href='./projects/train-scheduler/index.html' target='_blank'><img class='card-img-top' src='./assets/images/trainScheduler.jpg' alt='Card image cap'></a>" +
           "<div class='card-body'>" +
-            "<h5 class='card-title'>Card title</h5>" +
-            "<p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>" +
-          "</div>" +
-          "<div class='card-footer'>" +
-            "<small class='text-muted'>Last updated 3 mins ago</small>" +
-          "</div>" +
+            "<h5 class='card-title'>Train Scheduler</h5>" +
+              "</div>" +
+
         "</div>" +
         "<div class='card text-center' class='card'>" +
-          "<img class='card-img-top' src='...' alt='Card image cap'>" +
+          "<a href='./projects/triviagame/index.html' target='_blank'><img class='card-img-top' src='./assets/images/triviaGame.jpg' alt='Card image cap'></a>" +
           "<div class='card-body'>" +
-            "<h5 class='card-title'>Card title</h5>" +
-            "<p class='card-text'>This card has supporting text below as a natural lead-in to additional content.</p>" +
-          "</div>" +
-          "<div class='card-footer'>" +
-            "<small class='text-muted'>Last updated 3 mins ago</small>" +
-          "</div>" +
+            "<h5 class='card-title'>Trivia Game</h5>" +
+              "</div>" +
+
         "</div>" +
         "<div class='card text-center' class='card'>" +
-          "<img class='card-img-top' src='...' alt='Card image cap'>" +
+          "<a href='./projects/giphyassignment/index.html' target='_blank'><img class='card-img-top' src='./assets/images/giphy.jpg' alt='Card image cap'></a>" +
           "<div class='card-body'>" +
-            "<h5 class='card-title'>Card title</h5>" +
-            "<p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>" +
-          "</div>" +
-          "<div class='card-footer'>" +
-            "<small class='text-muted'>Last updated 3 mins ago</small>" +
-          "</div>" +
+            "<h5 class='card-title'>Giphy Assignment</h5>" +
+              "</div>" +
+
         "</div>" +
-      "</div>"
+      "</div>" +
+      "<br>"
       )
 });
 
 $("#contact").on('click', function(){
     $("#appendArea").empty();
     $("#appendArea").append(
+    "<h2 class='title'>Contact Me</h2>" +
+    "<br>" +
     "<section class='container'>" +
-    "<h1 class='aboutme'>" +
-        "Contact" +
-    "</h1>" +
-"<div>" +
+    "<div>" +
     "<form action='_blank'>" +
         "Name<br>" +
-        "<input type='text'>" +
+        "<input class='container' type='text'>" +
         "<br><br>" +
         "Email<br>" +
-        "<input type='text'>" +
+        "<input class='container' type='text'>" +
         "<br><br>" +
         "Message<br>" +
-        "<textarea id='textbox'></textarea>" +
+        "<textarea class='container' id='textbox'></textarea>" +
         "<br><br>" +
         "<input id='submitbutton' type='submit' value='Submit'>" +
     "</form>" +
